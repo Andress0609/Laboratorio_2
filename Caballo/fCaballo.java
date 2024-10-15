bf1c84af49177d40439c07c37926332c6cdb9964
Lesly Araya <lesarayamor@est.utn.ac.cr>
2024-10-15 16:17:56 -0600

    Se hacen nuevas agregaciones a la clase Caballo
    Se programan nuevas cosas como los movimiento etc

--- Caballo/fCaballo.java
+++ Caballo/fCaballo.java
@@ -36,5 +36,152 @@ public class fCaballo {
     public void setTablero(Tablero tablero) {
         this.tablero = tablero;
     }
+    
+    public void IniciarMovimiento(int fila, int columna) {
+
+        for (int i = 0; i < 8; i++) {
+            AsignarJugada(i);
+
+            fila = fila + UltimoMovimiento[0][0];
+            columna = columna + UltimoMovimiento[0][1];
+
+            if (IndicarMovimiento(fila, columna)) {
+                RealizarMovimiento(fila, columna);
+
+                IniciarMovimiento(fila, columna);
+                continue;
+            }
+            this.RegresarFilasColumnas(fila, columna, i);
+            fila = UltimoMovimiento[0][0];
+            columna = UltimoMovimiento[0][1];
+
+            UltimoMovimiento[0][0] = 0;
+            UltimoMovimiento[0][1] = 0;
+        }
+
+//        if (this.xIndicarMovimiento(x[0][0], x[0][1]) && x[0][0]!=0 && x[0][0]==0) {
+//            System.out.println("FILA "+x[0][0]+ " COLUMNA "+x[0][1]);
+//            this.tablero.tabla[x[0][0]][x[0][1]] = 0;
+//            Posicion--;
+//        }
+    }
+
+    public void DarPosicionesIniciales(int fila, int columna) {
+        RealizarMovimiento(fila, columna);
+
+        this.IniciarMovimiento(fila, columna);
+    }
+
+    public void AsignarJugada(int Indicador) {
+        switch (Indicador) {
+            case 0: {
+                UltimoMovimiento[0][0] = -2;
+                UltimoMovimiento[0][1] = 1;
+                break;
+            }
+
+            case 1: {
+
+                UltimoMovimiento[0][0] = -2;
+                UltimoMovimiento[0][1] = -1;
+                break;
+
+            }
+
+            case 2: {
+
+                UltimoMovimiento[0][0] = -1;
+                UltimoMovimiento[0][1] = -2;
+                break;
+
+            }
+
+            case 3: {
+
+                UltimoMovimiento[0][0] = 1;
+                UltimoMovimiento[0][1] = -2;
+                break;
+
+            }
+
+            case 4: {
+
+                UltimoMovimiento[0][0] = 2;
+                UltimoMovimiento[0][1] = -1;
+                break;
+
+            }
+
+            case 5: {
+
+                UltimoMovimiento[0][0] = 2;
+                UltimoMovimiento[0][1] = 1;
+                break;
+
+            }
+
+            case 6: {
+
+                UltimoMovimiento[0][0] = -1;
+                UltimoMovimiento[0][1] = 2;
+                break;
+
+            }
+
+            case 7: {
+
+                UltimoMovimiento[0][0] = 1;
+                UltimoMovimiento[0][1] = 2;
+                break;
+
+            }
+
+        }
+    }
+
+    public void RealizarMovimiento(int fila, int columna) {
+        Posicion++;
+        this.getTablero().tabla[fila][columna] = Posicion;
+    }
+
+    public void RegresarFilasColumnas(int fila, int columna, int x) {
+//        if (xIndicarMovimiento(UltimoMovimiento[0][0], UltimoMovimiento[0][1]) && x == 7) {
+//            this.tablero.tabla[UltimoMovimiento[0][0]][UltimoMovimiento[0][1]] = 0;
+//        }
+        UltimoMovimiento[0][0] = UltimoMovimiento[0][0] * -1;
+        UltimoMovimiento[0][1] = UltimoMovimiento[0][1] * -1;
+        UltimoMovimiento[0][0] = (fila + UltimoMovimiento[0][0]);
+        UltimoMovimiento[0][1] = (columna + UltimoMovimiento[0][1]);
+
+    }
+//
+//    public boolean xIndicarMovimiento(int fila, int columna) {
+//        try {
+//            if (tablero.getTabla()[fila][columna] != 0) {
+//                return true;
+//            } else {
+//                return false;
+//            }
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
+
+    public boolean IndicarMovimiento(int fila, int columna) {
+        try {
+            if (tablero.getTabla()[fila][columna] == 0) {
+                return true;
+            } else {
+
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+//        for (int i = 0; i < tablero.getTabla().length; i++) {
+//            
+//        }
 
 }
